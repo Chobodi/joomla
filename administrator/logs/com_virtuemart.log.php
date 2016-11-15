cafa826283b4b88c21a362b6cc47e5ba32a1719a
CHOBODI <CHOBODI>
2016-11-15 14:15:56 +0530

    first commit

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,7 @@
+#
+#<?php die("Forbidden."); ?>
+
+2016-11-14 04:17:05 ERROR vmError: Couldnt create thumb, file not found F:\wamp\www\Joomla_3.6.4-Stable-Full_Package\images\stories\virtuemart\category\images/stories/virtuemart/product/1.jpg
+2016-11-14 04:17:05 ERROR vmError: Couldnt create thumb, file not found F:\wamp\www\Joomla_3.6.4-Stable-Full_Package\images\stories\virtuemart\category\images/stories/virtuemart/product/1.jpg
+2016-11-14 04:19:05 ERROR vmError: Couldnt create thumb, file not found F:\wamp\www\Joomla_3.6.4-Stable-Full_Package\images\stories\virtuemart\category\images/stories/virtuemart/product/1.jpg
+2016-11-14 04:19:05 ERROR vmError: Couldnt create thumb, file not found F:\wamp\www\Joomla_3.6.4-Stable-Full_Package\images\stories\virtuemart\category\images/stories/virtuemart/product/1.jpg
---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-14 12:30:17 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-14T12:30:17+00:00	INFO ::1	joomlafailure	Empty password not allowed.
